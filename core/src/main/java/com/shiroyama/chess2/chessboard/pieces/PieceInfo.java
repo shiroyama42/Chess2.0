f511589068343df2374fa9cbf7e449f0ae9e5584
Vajas Benjámin <vajas.ben1227@gmail.com>
2025-02-24 12:12:03 +0100

    add hp and attack rate for every piece

--- core/src/main/java/com/shiroyama/chess2/chessboard/pieces/PieceInfo.java
+++ core/src/main/java/com/shiroyama/chess2/chessboard/pieces/PieceInfo.java
@@ -4,14 +4,42 @@ public class PieceInfo {
 
     public Team team;
     public PieceType pieceType;
+    public int hp;
+    public float attackRate;
 
     public PieceInfo(Team team, PieceType pieceType) {
         super();
         this.team = team;
         this.pieceType = pieceType;
+        this.hp = getBaseHP(pieceType);
+        this.attackRate = getAttackRate(pieceType);
     }
 
     public String getName(){
         return ((team == Team.WHITE) ? "white" : "black")+ "-" + pieceType.toString().toLowerCase();
+    }
+
+    private int getBaseHP(PieceType pieceType){
+        switch (pieceType){
+            case PAWN: return 1;
+            case KNIGHT: return 10;
+            case BISHOP: return 3;
+            case ROOK: return 10;
+            case QUEEN: return 15;
+            case KING: return 20;
+            default: return 1;
+        }
+    }
+
+    private float getAttackRate(PieceType pieceType){
+        switch (pieceType){
+            case PAWN: return 1f;
+            case KNIGHT: return 1.5f;
+            case BISHOP: return 2f;
+            case ROOK: return 2.5f;
+            case QUEEN: return 3f;
+            case KING: return 1f;
+            default: return 1f;
+        }
     }
 }
