45310c5bd27f918748ad6f0dde6fe84b1e56d7ea
Vajas Benjámin <vajas.ben1227@gmail.com>
2025-03-25 10:19:06 +0100

    add Javadoc for chessboard/utils package

--- core/src/main/java/com/shiroyama/chess2/chessboard/utils/AttackListener.java
+++ core/src/main/java/com/shiroyama/chess2/chessboard/utils/AttackListener.java
@@ -2,6 +2,17 @@ package com.shiroyama.chess2.chessboard.utils;
 
 import com.shiroyama.chess2.chessboard.pieces.PieceInfo;
 
+/**
+ * Interface for handling attack events in a chess game.
+ * Implementers of this interface will be notified when one piece
+ * attacks another piece on the board.
+ */
 public interface AttackListener {
+    /**
+     * Called when an attack occurs between two pieces.
+     *
+     * @param attacker the piece that is attacking
+     * @param defender the piece that is being attacked
+     */
     void onAttack(PieceInfo attacker, PieceInfo defender);
 }
--- core/src/main/java/com/shiroyama/chess2/chessboard/utils/PromotionListener.java
+++ core/src/main/java/com/shiroyama/chess2/chessboard/utils/PromotionListener.java
@@ -2,6 +2,16 @@ package com.shiroyama.chess2.chessboard.utils;
 
 import com.shiroyama.chess2.chessboard.pieces.PieceInfo;
 
+/**
+ * Interface for handling pawn promotion events in the chess game.
+ * Implementation of this interface will be notified when a pawn reaches
+ * the opposite end of the board and is eligible for promotion.
+ */
 public interface PromotionListener {
+    /**
+     * Called when a pawn is eligible for promotion.
+     *
+     * @param piece the pawn piece that is being promoted.
+     */
     void onPromote(PieceInfo piece);
 }
